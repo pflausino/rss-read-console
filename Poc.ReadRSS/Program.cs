ae1ad9a0f19bfdadb4406fc4ad122706becbfbff
pflausino <calderaro95@hotmail.com>
2020-12-17 17:59:50 -0300

    fist load

---
+++ Poc.ReadRSS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Poc.ReadRSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
